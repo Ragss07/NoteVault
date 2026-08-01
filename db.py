0131833627e8243fb485f7dcc2263e4e8f26bae4
Ragini <kanojiavinod907@gmail.com>
2026-08-02 04:17:30 +0530

    Dockerized Flask-MySQL application with Docker Compose

--- db.py
+++ db.py
@@ -1,10 +1,18 @@
 import mysql.connector
+import time
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="2027",
-    database="devops_db"
-)
+connection = None
 
-print("Database Connected Successfully!")
+while connection is None:
+    try:
+        connection = mysql.connector.connect(
+            host="mysql",
+            user="root",
+            password="2027",
+            database="devops_db"
+        )
+        print(" Connected to MySQL")
+
+    except mysql.connector.Error:
+        print(" MySQL not ready. Retrying in 5 seconds...")
+        time.sleep(5)
